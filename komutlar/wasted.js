826750ca75efd3809c6823c0442987ef99a00c8d
Mertsayar6623 <42028387+Mertsayar6623@users.noreply.github.com>
2018-08-25 23:37:17 +0300

    Add files via upload

---
+++ komutlar/wasted.js
@@ -0,0 +1,25 @@
+let prefix = "!!";
+var Jimp = require('jimp');
+
+client.on("message", async message => {
+  var user = message.mentions.users.first() || message.author;
+    if (message.content.toLowerCase() === prefix + "wasted") {
+      message.channel.startTyping();
+        var user = message.mentions.users.first() || message.author;
+        if (!message.guild) user = message.author;
+
+        Jimp.read(user.avatarURL, (err, image) => {
+            image.resize(295, 295)
+            image.greyscale()
+            image.gaussian(3)
+            Jimp.read("https://cdn.glitch.com/b18a2fa6-68cb-49d5-9818-64c50dd0fdab%2F1.png?1529363616039", (err, avatar) => {
+                avatar.resize(295, 295)
+                image.composite(avatar, 4, 0).write(`./img/wasted/${client.user.id}-${user.id}.png`);
+                setTimeout(function() {
+                    message.channel.send(new Discord.Attachment(`./img/wasted/${client.user.id}-${user.id}.png`));
+                }, 1000);
+          message.channel.stopTyping();
+            });
+        });
+    }
+});
